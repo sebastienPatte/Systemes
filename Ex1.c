7fd2af952454688d2fb3e6b7c88b1124a7907725
sebastienPatte <sebnb91@gmail.com>
2019-10-08 12:09:48 +0200

    fin Ex1 TP6

--- Ex1.c
+++ Ex1.c
@@ -18,7 +18,11 @@ dans la fonction main cette adresse ne sera donc pas celle du tableau
 */
 
 int *allouer_tableau(int dimension, int val){
-	int *t =  calloc(dimension,4);
+	int* t =  calloc(dimension,sizeof(int));
+	//si l'allocation mémoire a échoué, on arrète le programme
+	if(t == NULL){
+		exit(0);
+	}
 	for(int i=0; i<dimension; i++){
 		t[i]=val;
 	}
@@ -27,6 +31,7 @@ int *allouer_tableau(int dimension, int val){
 
 void liberer_tableau(int *tab){
 	free(tab);
+	tab=NULL;
 }
 
 int *lire_n_entiers(int n){
@@ -36,8 +41,6 @@ int *lire_n_entiers(int n){
 		printf("entrez un nb\n");
 		scanf("%d",&nb);
 		t[i]=nb;
-		printf("i=%d\n",i);
-
 	}
 	return t;
 }
@@ -50,11 +53,54 @@ void afficher_tableau(int *tab, int dimension){
 	printf("]\n");
 }
 
+int *lire_entiers(void){
+	int nb=1;
+	int cpt=0;
+	int* t=allouer_tableau(4,0);
+	while(nb!=0){
+		printf("entrez un nb ou terminez par 0\n");
+		scanf("%d",&nb);
+		t[cpt]=nb;
+		cpt++;
+		if(cpt%3 >= 3){
+			//le tableau contenait cpt-1 éléments donc on fait 2*(cpt-1) pour doubler sa taille
+			//en multipliant par sizeof(int) pour donner la taille en octets
+			t= realloc(t, 2*(cpt-1)*sizeof(int));
+		}
+	}
+	afficher_tableau(t,cpt-1);
+	return t;
+}
+
+int **allouer_matrice(int lignes, int colonnes, int val){
+	int **mat = malloc(lignes * sizeof(int *));
+	for(int i=0; i<lignes; i++){
+		mat[i]=allouer_tableau(colonnes,val);
+	}return mat;
+}
+
+void liberer_matrice(int **mat, int lignes){
+	for(int i=lignes-1; i>=0; i--){
+		liberer_tableau(mat[i]);
+	}free(mat);
+	mat=NULL;
+}
+
+void afficher_matrice(int **mat, int lignes, int colonnes){
+	for(int i=0;i<lignes;i++){
+		afficher_tableau(mat[i],colonnes);
+	}
+}
+
 int main(){
-	int taille = 3;
-	int *t = lire_n_entiers(taille);
-	afficher_tableau(t,taille);
+	int taille = 10;
+	int* t = lire_entiers();
 	liberer_tableau(t);
-	printf("%d",t[0]);
+	afficher_tableau(t,taille);
+//	int** mat = allouer_matrice(10,10,8);
+//	afficher_matrice(mat,10,10);
+//	liberer_matrice(mat,10);
+//	afficher_matrice(mat,10,10);
+
 }
 
