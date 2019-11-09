865f4794215a92a679fe027b8c3b1050adc69345
sebastienPatte <sebnb91@gmail.com>
2019-11-09 16:17:25 +0100

    comments & error managing in functions 'copie' & 'copieDossier'

--- TP8/Ex1.c
+++ TP8/Ex1.c
@@ -10,53 +10,65 @@
 
 void copie(char* path1, char* path2, int size)
 {      
-	
+	/*on ouvre le fichier à l'emplacement 'path1' en lecture seule
+	  on récupère le file descriptor f1*/
     int f1 = open(path1, O_RDONLY);  
-      
-    //printf("f1 = %d\n", f1); 
-      
     if (f1 ==-1) 
     { 
-        perror("Error opening file1");  
-        printf("path = \"%s\"\n",path1);
-                            
+        perror("Error opening file1 (function 'copie') ");  
+        printf("when path = \"%s\"\n",path1);
     }
     
-    int f2 = open(path2, O_WRONLY | O_CREAT);  
-      
-    //printf("f2 = %d\n", f2); 
-      
+    /*on ouvre le fichier à l'emplacement 'path2' en lecture/écriture 
+      si le fichier n'existe pas, il est créé
+	  on récupère le file descriptor f2*/
+    int f2 = open(path2, O_WRONLY | O_CREAT);
     if (f2 ==-1) 
     { 
-        perror("Error opening file2");  
-        printf("path = \"%s\"\n",path2);
-                            
+        perror("Error opening file2 (function 'copie') ");  
+        printf("when path = \"%s\"\n",path2);
     }
     
     int sizeOfRead;
     char buf[size];
     do
     {    
-    
-    sizeOfRead = read(f1,buf,size);	
+    	/*copie de blocs de taille maximale 'size' de f1 vers f2
+    	  on arrète quand on a lu un bloc plus petit que size (car fin du fichier)*/
+	    sizeOfRead = read(f1,buf,size);	
+		write(f2,buf,sizeOfRead);
+	}while( !(sizeOfRead < size));
 	
-	write(f2,buf,sizeOfRead);
-	
-	}while(sizeOfRead !=0);
-	
+	//on récupère la structure stat pour f1 dans le buffer 'statBuf'
 	struct stat statBuf;
-	fstat(f1,&statBuf);
-	//S_IRWX is a mask for Read Write Read permissions, finished by U for user, G for group and O for Others
+	if(fstat(f1,&statBuf) == -1)
+	{
+		perror("Error fstat file1 (function 'copie') ");
+	}
+	
+	//on récupère dans 'file_permission' les permissions du fichier à partier de statBuf et grâce au masque S_IRWX[U|G|O]  
 	mode_t file_permision = statBuf.st_mode & (S_IRWXU | S_IRWXG | S_IRWXO); 
-   	//printf(" File permission : %o (octal)\n", (unsigned int) file_permision); 
-   	chmod(path2,file_permision);
-	close(f1);
-	close(f2);
+   	//on donne ces permissions à f2
+   	if(chmod(path2,file_permision) == -1)
+   	{
+   		perror("Error chmod file2 (function 'copie') ");	
+   	}
+
+   	//on ferme f1 et f2
+	if(close(f1) == -1)
+	{
+		perror("Error closing file1 (function 'copie') ");
+	}
+	if(close(f2) == -1)
+	{
+		perror("Error closing file2 (function 'copie') ");
+	}
 } 
 
 char* concatStr(char* s1, char* s2)
-{
+{	// on concatène deux char* en copiant s1 dans res puis en y ajoutant s2
 	char* res;
+	//res a la taille de s1+s2
 	res = calloc(strlen(s1)+strlen(s2), sizeof(char));
 	strcpy(res,s1);
 	strcat(res,s2);
@@ -66,11 +78,14 @@ char* concatStr(char* s1, char* s2)
 void copieDossier(char* path1, char* path2, int size)
 {
 	struct dirent *srcDirent;
-	struct stat stElt;
+	struct stat bufElt;
 	DIR* dirSrc;
 	dirSrc = opendir(path1);
-
-	if(dirSrc !=NULL)
+	if(dirSrc ==NULL)
+	{
+		perror("Error opendir path1 (function copieDossier) ");
+	}
+	else
 	{
 		while((srcDirent = readdir(dirSrc)) != NULL)
 		{
@@ -81,7 +96,7 @@ void copieDossier(char* path1, char* path2, int size)
 			{
 
 				//printf("fileName = |%s|\n", fileName);
-				// ajout des "/" à la fin des noms de dossier
+				// ajout des "/" à la fin des noms de dossier pour ceux qui n'en ont pas
 				if (strlen(path1)-1>=0 && path1[strlen(path1)-1]!='/')
 				{
 					path1 = concatStr(path1,"/");
@@ -91,12 +106,14 @@ void copieDossier(char* path1, char* path2, int size)
 					path2 = concatStr(path2,"/");
 				}
 				
-				if(stat(concatStr(path1,fileName),&stElt)==-1)
+				// on récupère la structure stat pour l'élément du répertoire dans le buffer bufElt
+				if(stat(concatStr(path1,fileName),&bufElt)==-1)
 				{
 					perror("Error stat ");
 				}
 
-				if(S_ISREG(stElt.st_mode))
+				//on utilise la macro S_ISREG pour voir si l'élément est un fichier
+				if(S_ISREG(bufElt.st_mode))
 				{//si l'élément est un fichier, on le copie
 					copie(concatStr(path1,fileName), concatStr(path2,fileName), size);	
 				}
@@ -104,13 +121,17 @@ void copieDossier(char* path1, char* path2, int size)
 			}
 		}
 	}
-	closedir(dirSrc);
+	if(closedir(dirSrc) == -1)
+	{
+		perror("Error closedir path1 (function copieDossier) ");
+		printf("when path = %s\n", path1);
+	}
 }
 
 void copieRecursive(char* path1, char* path2, int size)
 {
 	struct dirent *srcDirent;
-	struct stat stElt;
+	struct stat bufElt;
 	DIR* dirSrc;
 	dirSrc = opendir(path1);
 
@@ -124,13 +145,13 @@ void copieRecursive(char* path1, char* path2, int size)
 			{ // on ignore le dossier courant et le dossier parent
 				
 				printf("path1+elt = %s %s\n", path1,pathElt);
-				if(stat(concatStr(path1,pathElt),&stElt)==-1)
+				if(stat(concatStr(path1,pathElt),&bufElt)==-1)
 				{
 					perror("Error stat ");
 				}
 				
 		
-				if(S_ISDIR(stElt.st_mode))
+				if(S_ISDIR(bufElt.st_mode))
 				{ // si Elt est un dossier
 					if (strlen(pathElt)-1>=0 && pathElt[strlen(pathElt)-1]!='/')
 					{
